829d6bdb99b94bdc7d38ddd216239093f13801b7
Ardhanpratama <fikriardhan01@gmail.com>
2025-10-22 16:50:53 +0700

    update

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program Java Sederhana untuk Latihan Git");
+        System.out.println("Dibuat oleh: Fikri Ardhan Putra Pratama");
+        System.out.println("NIM: 202410370110375");
+    }
+}
